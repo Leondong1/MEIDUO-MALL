aefec953522375c6c1d157712c28c65e9e317920
Leon <wangdongjie1994@gmail.com>
2019-06-24 07:53:51 +0800

    QQ第三方登录功能实现

---
+++ meiduo_mall/meiduo_mall/apps/oauth/constants.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2019-06-23 19:44
+@Author  : Leon
+@Contact : wangdongjie1994@gmail.com
+@File    : constants.py
+@Software: PyCharm
+'''
+# 绑定用户的有效期
+BIND_USER_ACCESS_TOKEN_EXPIRES = 10 * 60
---
+++ meiduo_mall/meiduo_mall/apps/oauth/exceptions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2019-06-23 18:11
+@Author  : Leon
+@Contact : wangdongjie1994@gmail.com
+@File    : exceptions.py
+@Software: PyCharm
+'''
+class OAuthQQAPIError(Exception):
+    pass
